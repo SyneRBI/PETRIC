12ff90a77d1e5defc92d5556c13d8116c11bea1d
Evgueni Ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2024-03-01 19:02:36 +0000

    tried to handle normalisation data, hit the problem with os.system('sed ...')

--- src/SIRF_data_preparation/try_data_preparation.py
+++ src/SIRF_data_preparation/try_data_preparation.py
@@ -7,8 +7,9 @@ from sirf_exercises import exercises_data_path
 from sirf.Utilities import examples_data_path
 
 data_path = exercises_data_path('PET', 'mMR', 'NEMA_IQ')
-#challenge_data_path = os.path.join(repo_directory, 'data')
-challenge_data_path = os.path.join(repo_directory, 'tmp')
+# challenge_data_path = os.path.join(repo_directory, 'data')
+# for debugging purposes using tmp1 subfolder instead of data
+challenge_data_path = os.path.join(repo_directory, 'tmp1')
 
 listmode_filename = os.path.join(data_path, '20170809_NEMA_60min_UCL.l.hdr')
 template_filename = os.path.join(examples_data_path('PET'), 'mMR', 'mMR_template_span11_small.hs')
@@ -23,3 +24,13 @@ stir_attn_header = os.path.join(challenge_data_path, '20170809_NEMA_MUMAP_UCL.hv
 acq_sens_filename = os.path.join(challenge_data_path, 'acf.hs')
 os.system('cp ' + siemens_attn_image + ' ' + challenge_data_path)
 acquisition_sensitivity_from_attenuation(siemens_attn_header, template_filename, stir_attn_header, acq_sens_filename)
+
+siemens_norm = os.path.join(data_path, '20170809_NEMA_UCL.n')
+os.system('cp ' + siemens_norm + ' ' + challenge_data_path)
+siemens_norm_header = os.path.join(data_path, '20170809_NEMA_UCL.n.hdr')
+os.system('cp ' + siemens_norm_header + ' ' + challenge_data_path)
+stir_norm_header = os.path.join(challenge_data_path, 'norm.n.hdr')
+os.system('cp ' + siemens_norm_header + ' ' + stir_norm_header)
+# cmd = 'sed -i.bak "s/\r\([^\n]\)/\r\n\1/g" norm.n.hdr'
+# fails with the error message
+# sed: -e expression #1, char 7: unterminated `s' command
