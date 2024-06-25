ab32faafdb108edb2a2a8b65309a69ae75134c41
Kris Thielemans <k.thielemans@ucl.ac.uk>
2024-06-25 23:57:57 +0100

    improve BSREM_common.py
    - add more options
    - save/plot at save_interval
    - normalise images for tensorboard

--- SIRF_data_preparation/BSREM_common.py
+++ SIRF_data_preparation/BSREM_common.py
@@ -1,11 +1,15 @@
+'''Functions to run BSREM with some monitoring via TensorBoard.'''
+# Authors: Kris Thielemans, Casper da Costa Luis
+# Licence: Apache-2.0
+# Copyright (C) 2024 University College London
+# Copyright (C) 2024 STFC, UK Research and Innovation
+
 import sirf.STIR as STIR
 from sirf.contrib.BSREM.BSREM import BSREM1  # , BSREM2
 from cil.optimisation.utilities import callbacks
 from cil.optimisation.algorithms import Algorithm
-# Import functionality from the Python files in SIRF-Contribs. (Note that in most set-ups, this will be from the installed files.)
+# Import functionality from the Python files in SIRF-Contribs
 from sirf.contrib.partitioner import partitioner
-#from sirf.Utilities import examples_data_path
-#from sirf_exercises import exercises_data_path
 from tensorboardX import SummaryWriter
 
 
@@ -42,29 +46,36 @@ def add_prior_to_obj_funs(obj_funs, prior, initial_image):
 
 
 class TensorBoardCallback(callbacks.Callback):
-    ITERS = 80, 600, 660  # iters to log. NB: final iteration triggers StopIteration
 
-    def __init__(self, verbose=1, im_slice=None, logdir=None):
+    def __init__(self, verbose=1, transverse_slice=None, coronal_slice=None, vmax=None, logdir=None):
         super().__init__(verbose)
         self.im = {}
-        self.slice = im_slice
+        self.transverse_slice = transverse_slice
+        self.coronal_slice = coronal_slice
         self.dims = None
+        self.vmax = vmax
+        self.prev = None
         self.matplotlib = False
         self.tb = SummaryWriter(logdir=logdir)
 
     def __call__(self, algo: Algorithm):
-        if algo.iteration in self.ITERS:
-            algo.x.write(f'{self.tb.logdir}/iter_{algo.iteration:04d}.hv')
+        if algo.iteration % algo.update_objective_interval == 0 or algo.iteration == algo.max_iteration:
+            self.tb.add_scalar("objective", algo.get_last_loss(), algo.iteration)
+            if self.prev:
+                normalised_change = (algo.x - self.prev).norm() / algo.x.norm()
+                self.tb.add_scalar("normalised_change", normalised_change, algo.iteration)
+                
+            algo.x.write(f'{self.tb.logdir}/BSREM_{algo.iteration:04d}.hv')
             self.im[algo.iteration] = algo.x.clone()
             self.dims = self.dims or algo.x.dimensions()
-            im_slice = self.dims[0] // 2 if self.slice is None else self.slice
-            cor_slice = self.dims[1] // 2
-            self.tb.add_image("transverse", algo.x.as_array()[im_slice:im_slice+1], algo.iteration)
-            self.tb.add_image("coronal", algo.x.as_array()[:, cor_slice][None, :], algo.iteration)
-        if algo.iteration % algo.update_objective_interval == 0:
-            self.tb.add_scalar("objective", algo.get_last_loss(), algo.iteration)
-        if algo.iteration != self.ITERS[-1]:
-            return
+            self.vmax = self.vmax or algo.x.max()
+            transverse_slice = self.dims[0] // 2 if self.transverse_slice is None else self.transverse_slice
+            cor_slice = self.dims[1] // 2 if self.coronal_slice is None else self.coronal_slice
+            
+            self.tb.add_image("transverse", algo.x.as_array()[transverse_slice:transverse_slice+1] / self.vmax, algo.iteration)
+            self.tb.add_image("coronal", algo.x.as_array()[:, cor_slice][None, :] / self.vmax, algo.iteration)
+
+            self.prev = algo.x.clone()
 
         # final iteration
         if not self.matplotlib:
@@ -73,13 +84,13 @@ class TensorBoardCallback(callbacks.Callback):
         import matplotlib.pyplot as plt
         plt.figure()
         plt.subplot(141)
-        plt.imshow(self.im[80].as_array()[im_slice, :, :], vmax=cmax)
+        plt.imshow(self.im[80].as_array()[transverse_slice, :, :], vmax=cmax)
         plt.subplot(142)
-        plt.imshow(self.im[600].as_array()[im_slice, :, :], vmax=cmax)
+        plt.imshow(self.im[600].as_array()[transverse_slice, :, :], vmax=cmax)
         plt.subplot(143)
-        plt.imshow(self.im[660].as_array()[im_slice, :, :], vmax=cmax)
+        plt.imshow(self.im[660].as_array()[transverse_slice, :, :], vmax=cmax)
         plt.subplot(144)
-        plt.imshow((self.im[600] - self.im[660]).as_array()[im_slice, :, :], vmin=-cmax / 20, vmax=cmax / 20)
+        plt.imshow((self.im[600] - self.im[660]).as_array()[transverse_slice, :, :], vmin=-cmax / 20, vmax=cmax / 20)
         plt.savefig('BSREM_transverse_images.png')
         plt.figure()
         plt.subplot(141)
@@ -106,10 +117,12 @@ class TensorBoardCallback(callbacks.Callback):
         raise StopIteration
 
 
-def run(num_subsets: int = 7, im_slice: int | None = None):
+def run(num_subsets: int = 7, num_updates: int = 5000, save_interval:int = 10, transverse_slice: int | None = None, coronal_slice: int | None = None):
     """
     num_subsets: number of subsets to use
-    im_slice: slice to show in the middle of the image (default: len(image) // 2)
+    num_updates: number of updates ("iterations" in CIL terminology) to use
+    transverse_slice: slice to show for transverse image (default: len(image) // 2)
+    coronal_slice: slice to show for the coronal image (default: len(image[0]) // 2)
     """
     # Needed for get_subsets()
     STIR.AcquisitionData.set_storage_scheme('memory')
@@ -132,9 +145,9 @@ def run(num_subsets: int = 7, im_slice: int | None = None):
                                                             mult_factors, num_subsets,
                                                             initial_image=OSEM_image)
     add_prior_to_obj_funs(obj_funs, prior, OSEM_image)
-    bsrem1 = BSREM1(data, obj_funs, initial=OSEM_image, initial_step_size=.3, relaxation_eta=.05,
-                    update_objective_interval=10)
-    bsrem1.run(iterations=661, callbacks=[
+    bsrem1 = BSREM1(data, obj_funs, initial=OSEM_image, initial_step_size=.3, relaxation_eta=.01,
+                    update_objective_interval=save_interval)
+    bsrem1.run(iterations=num_updates, callbacks=[
         callbacks.ProgressCallback(),
-        TensorBoardCallback(im_slice=im_slice) # WARNING: should specify logdir here to avoid writing to data source dir
+        TensorBoardCallback(transverse_slice=transverse_slice, coronal_slice=coronal_slice) # WARNING: should specify logdir here to avoid writing to data source dir
     ])
