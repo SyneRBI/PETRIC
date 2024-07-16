0df022434eaec565e3f6e84dd6b6e2340b251cb5
Kris Thielemans <k.thielemans@ucl.ac.uk>
2024-07-16 07:33:49 +0000

    update BSREM ref generation scripts

--- SIRF_data_preparation/BSREM_NeuroLF_Hoffman.py
+++ SIRF_data_preparation/BSREM_NeuroLF_Hoffman.py
@@ -1,9 +1,21 @@
-from petric import MetricsWithTimeout, get_data
+from petric import MetricsWithTimeout, get_data, SRCDIR
 from sirf.contrib.BSREM.BSREM import BSREM1
 from sirf.contrib.partitioner import partitioner
 
-data = get_data(srcdir="./data/NeuroLF_Hoffman_Dataset", outdir="./output/BSREM_NeuroLF_Hoffman")
-num_subsets = 16
+scanID = "NeuroLF_Hoffman_Dataset"
+if scanID == 'Siemens_mMR_NEMA_IQ':
+    slices = { 'transverse_slice': 72, 'coronal_slice': 109} #, 'sagittal_slice': 89}
+    num_subsets = 7
+elif scanID == 'NeuroLF_Hoffman_Dataset':
+    slices = { 'transverse_slice': 72}
+    num_subsets = 16
+else:
+    slices = {}
+    # Vision
+    num_subsets = 5
+
+outdir="./output/BSREM_" + scanID
+data = get_data(srcdir=SRCDIR / scanID, outdir=outdir)
 data_sub, acq_models, obj_funs = partitioner.data_partition(data.acquired_data, data.additive_term, data.mult_factors,
                                                             num_subsets, initial_image=data.OSEM_image)
 # WARNING: modifies prior strength with 1/num_subsets (as currently needed for BSREM implementations)
@@ -13,5 +25,6 @@ for f in obj_funs: # add prior evenly to every objective function
     f.set_prior(data.prior)
 
 algo = BSREM1(data_sub, obj_funs, initial=data.OSEM_image, initial_step_size=.3, relaxation_eta=.01,
-              update_objective_interval=10)
-algo.run(5000, callbacks=[MetricsWithTimeout(transverse_slice=72)])
+              update_objective_interval=80)
+#%%
+algo.run(15000, callbacks=[MetricsWithTimeout(**slices, outdir=outdir, seconds=3600*100)])
--- SIRF_data_preparation/BSREM_Vision600_thorax.py
+++ SIRF_data_preparation/BSREM_Vision600_thorax.py
@@ -1,9 +1,23 @@
-from petric import MetricsWithTimeout, get_data
+from petric import MetricsWithTimeout, get_data, SRCDIR
 from sirf.contrib.BSREM.BSREM import BSREM1
 from sirf.contrib.partitioner import partitioner
+import sirf.STIR as STIR
+import os
 
-data = get_data(srcdir="./data/Siemens_Vision600_thorax", outdir="./output/BSREM_Vision600_thorax")
-num_subsets = 5
+if scanID == 'Siemens_mMR_NEMA_IQ':
+    slices = { 'transverse_slice': 72, 'coronal_slice': 109} # , 'sagittal_slice': 89}
+    num_subsets = 7
+elif scanID == 'NeuroLF_Hoffman_Dataset':
+    slices = { 'transverse_slice': 72}
+    num_subsets = 16
+else:
+    slices = {}
+    # Vision
+    num_subsets = 5
+
+outdir="./output/BSREM_" + scanID
+outdir1="./output/BSREM_" + scanID + "_cont"
+data = get_data(srcdir=SRCDIR / scanID, outdir=outdir)
 data_sub, acq_models, obj_funs = partitioner.data_partition(data.acquired_data, data.additive_term, data.mult_factors,
                                                             num_subsets, initial_image=data.OSEM_image)
 # WARNING: modifies prior strength with 1/num_subsets (as currently needed for BSREM implementations)
@@ -12,6 +26,18 @@ data.prior.set_up(data.OSEM_image)
 for f in obj_funs: # add prior evenly to every objective function
     f.set_prior(data.prior)
 
-algo = BSREM1(data_sub, obj_funs, initial=data.OSEM_image, initial_step_size=.3, relaxation_eta=.01,
-              update_objective_interval=10)
-algo.run(5000, callbacks=[MetricsWithTimeout()])
+OSEM_image = data.OSEM_image
+# clean-up some data, now that we have the subsets
+del data
+try:
+    image1000 = STIR.ImageData(os.path.join(outdir, 'iter_1000.hv'))
+except:
+    algo = BSREM1(data_sub, obj_funs, initial=OSEM_image, initial_step_size=.3, relaxation_eta=.01,
+                  update_objective_interval=5)
+    algo.run(1005, callbacks=[MetricsWithTimeout(seconds=3600*34, outdir=outdir)])
+
+# restart
+
+algo = BSREM1(data_sub, obj_funs, initial=image1000, initial_step_size=.3, relaxation_eta=.01,
+              update_objective_interval=5)
+algo.run(9000, callbacks=[MetricsWithTimeout(seconds=3600*34, outdir=outdir1)])
