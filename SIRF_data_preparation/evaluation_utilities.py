d1044ff6f512e88b23e9436c9f23945ee3a2f3d7
Casper da Costa-Luis <casper.dcl@physics.org>
2024-07-17 06:36:54 +0000

    pass_index

--- SIRF_data_preparation/evaluation_utilities.py
+++ SIRF_data_preparation/evaluation_utilities.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Iterator
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
 import sirf.STIR as STIR
 from petric import QualityMetrics
@@ -15,16 +15,31 @@ def read_objectives(datadir='.'):
     with (Path(datadir) / 'objectives.csv').open() as csvfile:
         reader = csv.reader(csvfile)
         next(reader) # skip first (header) line
-        return numpy.asarray([tuple(map(float, row)) for row in reader])
+        return np.asarray([tuple(map(float, row)) for row in reader])
 
 
 def get_metrics(qm: QualityMetrics, iters: Iterator[int], srcdir='.'):
     """Read 'iter_{iter_glob}.hv' images from datadir, compute metrics and return as 2d array"""
-    return numpy.asarray([
+    return np.asarray([
         list(qm.evaluate(STIR.ImageData(str(Path(srcdir) / f'iter_{i:04d}.hv'))).values()) for i in iters])
 
 
-def plot_metrics(iters: Iterator[int], m: numpy.ndarray, labels=None, suffix=""):
+def pass_index(metrics: np.ndarray, thresh: np.ndarray, window: int = 1) -> int:
+    """
+    Returns first index of `metrics` with value <= `thresh`.
+    The value must remain below the threshold for at least `window`.
+    Raises IndexError if doesn't pass.
+    """
+    assert metrics.shape[1] == len(thresh)
+    assert metrics.ndim == 2
+    assert thresh.ndim == 1
+
+    m = (metrics <= thresh[None]).all(1)
+    assert window == 1
+    return np.where(m[1:] & m[:-1])[0][0]
+
+
+def plot_metrics(iters: Iterator[int], m: np.ndarray, labels=None, suffix=""):
     """Make 2 subplots of metrics"""
     if labels is None:
         labels = [""] * m.shape[1]
