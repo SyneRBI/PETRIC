52272ff9847219bb30b215c16c2fb51275006f37
Casper da Costa-Luis <casper.dcl@physics.org>
2024-10-02 14:26:04 +0100

    lint

--- SIRF_data_preparation/get_penalisation_factor.py
+++ SIRF_data_preparation/get_penalisation_factor.py
@@ -14,24 +14,26 @@ Options:
 # Copyright 2024 University College London
 # Licence: Apache-2.0
 
+import sys
+from pathlib import Path
+
+import numpy as np
+from docopt import docopt
+
 # %% imports
 import sirf.STIR
-import numpy as np
-import sys
-from docopt import docopt
-from pathlib import Path
-from petric import get_data, Dataset
+from petric import Dataset, get_data
 from SIRF_data_preparation.data_utilities import the_data_path
 
 # %%
 __version__ = "0.1.0"
 
 write_penalisation_factor = False
-if (
-    not "ipykernel" in sys.argv[0]
-):  # clunky way to be able to set variables from within jupyter/VScode without docopt
+if "ipykernel" not in sys.argv[0]: # clunky way to be able to set variables from within jupyter/VScode without docopt
     args = docopt(__doc__, argv=None, version=__version__)
+
     # logging.basicConfig(level=logging.INFO)
+
     dataset = args["--dataset"]
     ref_dataset = args["--ref_dataset"]
     if dataset is None or ref_dataset is None:
@@ -39,11 +41,9 @@ if (
         exit(1)
     if args["--write_penalisation_factor"] is not None:
         write_penalisation_factor = False
-else:
-    # set it by hand, e.g.
+else:         # set it by hand, e.g.
     ref_dataset = "NeuroLF_Hoffman_Dataset"
     dataset = "Siemens_mMR_NEMA_IQ"
-
     write_penalisation_factor = True
 
 
@@ -62,9 +62,7 @@ def backgroundVOImean(dataset: Path) -> float:
 def get_penalisation_factor(ref_data: Dataset, cur_data: Dataset) -> float:
     ref_mean = VOImean(ref_data.OSEM_image, ref_data.background_mask)
     cur_mean = VOImean(cur_data.OSEM_image, cur_data.background_mask)
-    print(
-        f"ref_mean={ref_mean}, cur_mean={cur_mean}, c/r={cur_mean / ref_mean}, r/c={ref_mean / cur_mean}"
-    )
+    print(f"ref_mean={ref_mean}, cur_mean={cur_mean}, c/r={cur_mean / ref_mean}, r/c={ref_mean / cur_mean}")
     ref_penalisation_factor = ref_data.prior.get_penalisation_factor()
     penalisation_factor = ref_penalisation_factor * cur_mean / ref_mean
     print(f"penalisation_factor={penalisation_factor}")
